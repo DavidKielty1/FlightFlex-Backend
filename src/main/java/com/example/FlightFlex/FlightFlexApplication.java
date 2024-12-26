b6a86fb93083e4cd598a4327e11738610507ee96
DavidKielty1 <78271797+DavidKielty1@users.noreply.github.com>
2024-12-26 18:44:55 +0000

    First commit

---
+++ src/main/java/com/example/FlightFlex/FlightFlexApplication.java
@@ -0,0 +1,13 @@
+package com.example.FlightFlex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightFlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightFlexApplication.class, args);
+	}
+
+}
